16ee58d7cb255d59b772fdb7282cde2c5bd10ece
sonabisure <116837107+sonabisure@users.noreply.github.com>
2023-04-18 06:32:10 -0700

    Add files via upload

---
+++ SportsManangementApplication.java
@@ -0,0 +1,14 @@
+package com.sports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsManangementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsManangementApplication.class, args);
+		System.out.println("SportManagement Application is running..");
+	}
+
+}
